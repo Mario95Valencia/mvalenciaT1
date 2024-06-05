7cc7e9ff6a481b8e1e15dfdd9070b57100e55ed3
Mario Valencia <mariodavid_18@hotmail.es>
2024-06-04 21:22:22 -0500

    calculo aporte

--- mvalenciaT1/View/vHome.xaml.cs
+++ mvalenciaT1/View/vHome.xaml.cs
@@ -10,7 +10,7 @@ public partial class vHome : ContentPage
     private void btnGenerar_Clicked(object sender, EventArgs e)
     {
         double salario = Convert.ToDouble(txtSalario.Text);
-        double aporte = salario / 0.0945;
+        double aporte = salario * 0.0945;
         DisplayAlert("Mensaje", "Bienvenido" + txtNombre.Text + " " + txtApellido.Text + "\n Tienes " + txtEdad.Text + " años \n Tu aporte mensual es: " + aporte, "Gracias");
     }
 }
